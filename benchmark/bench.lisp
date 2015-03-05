d5a763cfee149a6d42f6272b265f984da5d7c310
Sunrim KIM (keen) <3han5chou7@gmail.com>
2015-03-05 10:33:35 +0900

    add repeat benchmark

--- benchmark/bench.lisp
+++ benchmark/bench.lisp
@@ -11,7 +11,7 @@ Copyright (c) 2014 κeen
    :cl
    :arrows)
   (:import-from :fast-io
-   :with-fast-output)
+                :with-fast-output)
   (:import-from :alexandria
                 :make-keyword))
 (in-package :arrows-bench)
@@ -19,7 +19,10 @@ Copyright (c) 2014 κeen
 (defparameter *templates* `(("simple.tmpl" . ())
                             ("1var.tmpl" . (:name "κeen"))
                             ("100var.tmpl" . ,(loop :for i :from 1 :to 100
-                                                    :append (list (make-keyword (format nil "FOO~a" i)) (format nil "bar~a" i))))))
+                                                 :append (list (make-keyword (format nil "FOO~a" i)) (format nil "bar~a" i))))
+                            ("repeat.tmpl" . (:foos ',(loop :for i :from 1 :to 100
+                                                         :collect (format nil "foo~a" i))))))
+
 
 (defmacro bench10000 (title form)
   `(progn
@@ -33,22 +36,22 @@ Copyright (c) 2014 κeen
         (octets-renderer (compile-template-file :octets tmpl ()))
         (fast-io-renderer (compile-template-file :fast-io tmpl ())))
     `(progn
-      (with-open-file (/dev/null "/dev/null" :direction :output :if-exists :append)
-        (bench10000 (format nil "compiled stream backend with ~a" ,tmpl)
-                      (funcall ,stream-renderer /dev/null ,@args))
-        (bench10000 (format nil "compiled string backend with ~a" ,tmpl)
-                      (write-string (funcall ,string-renderer ,@args) /dev/null)))
-      (with-open-file (/dev/null "/dev/null" :element-type '(unsigned-byte 8) :direction :output :if-exists :append)
-        (bench10000 (format nil "compiled octet stream backend with ~a" ,tmpl)
-                      (funcall  ,octet-stream-renderer /dev/null ,@args))
-        (bench10000 (format nil "compiled octet backend with ~a" ,tmpl)
-                      (write-sequence (funcall ,octets-renderer ,@args) /dev/null))
-        (bench10000 (format nil "compiled fast-io backend with ~a" ,tmpl)
-                      (with-fast-output (buff /dev/null) (funcall ,fast-io-renderer buff ,@args)))))))
+       (with-open-file (/dev/null "/dev/null" :direction :output :if-exists :append)
+         (bench10000 (format nil "compiled stream backend with ~a" ,tmpl)
+                     (funcall ,stream-renderer /dev/null ,@args))
+         (bench10000 (format nil "compiled string backend with ~a" ,tmpl)
+                     (write-string (funcall ,string-renderer ,@args) /dev/null)))
+       (with-open-file (/dev/null "/dev/null" :element-type '(unsigned-byte 8) :direction :output :if-exists :append)
+         (bench10000 (format nil "compiled octet stream backend with ~a" ,tmpl)
+                     (funcall  ,octet-stream-renderer /dev/null ,@args))
+         (bench10000 (format nil "compiled octet backend with ~a" ,tmpl)
+                     (write-sequence (funcall ,octets-renderer ,@args) /dev/null))
+         (bench10000 (format nil "compiled fast-io backend with ~a" ,tmpl)
+                     (with-fast-output (buff /dev/null) (funcall ,fast-io-renderer buff ,@args)))))))
 
 (defmacro bench (tmpl args)
   `(bench/arrows ,tmpl ,args))
 
 #.`(progn
-    ,@(loop :for (tmpl . args) :in *templates*
+     ,@(loop :for (tmpl . args) :in *templates*
           :collect `(bench ,tmpl ,args)))
