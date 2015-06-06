dfef9f9a6ff0ca37c8c979e2a831a970e5c8658e
Sunrim KIM (keen) <3han5chou7@gmail.com>
2015-06-06 17:28:02 +0900

    change test cases to follow up new lexer/parser

--- t/parse.lisp
+++ t/parse.lisp
@@ -21,21 +21,30 @@ Copyright (c) 2014 κeen
     :test #'att-equal)
 
 (is (parse-template-string "{{var sym}}")
-    (att-progn (att-output (att-variable 'sym)))
+    (att-progn
+     (att-output (att-string ""))
+     (att-output (att-variable 'sym))
+     (att-output (att-string "")))
     "var"
     :test #'att-equal)
 
 (is (parse-template-string "{{if cond}}aaa{{endif}}")
-    (att-progn (att-if (att-variable 'cond)
-                       (att-progn (att-output (att-string "aaa")))
-                       (att-nil)))
+    (att-progn
+     (att-output (att-string ""))
+     (att-if (att-variable 'cond)
+             (att-progn (att-output (att-string "aaa")))
+             (att-nil))
+     (att-output (att-string "")))
     "if"
     :test #'att-equal)
 
 (is (parse-template-string "{{if cond}}aaa{{else}}bbb{{endif}}")
-    (att-progn (att-if (att-variable 'cond)
-                       (att-progn (att-output (att-string "aaa")))
-                       (att-progn (att-output (att-string "bbb")))))
+    (att-progn
+     (att-output (att-string ""))
+     (att-if (att-variable 'cond)
+             (att-progn (att-output (att-string "aaa")))
+             (att-progn (att-output (att-string "bbb"))))
+     (att-output (att-string "")))
     "if else"
     :test #'att-equal)
 
@@ -47,35 +56,50 @@ Copyright (c) 2014 κeen
     :test #'att-equal)
 
 (is (parse-template-string "{{repeat 10 as i}}<li>item{{var i}}</li>{{endrepeat}}")
-    (att-progn (att-loop (att-constant '(1 2 3 4 5 6 7 8 9 10))
+    (att-progn
+     (att-output (att-string ""))
+     (att-loop (att-constant '(1 2 3 4 5 6 7 8 9 10))
                          (att-progn (att-output (att-string "<li>item"))
                                     (att-output (att-variable 'i))
                                     (att-output (att-string "</li>")))
-                         (att-variable 'i)))
+                         (att-variable 'i))
+     (att-output (att-string "")))
     "repeat with index"
     :test #'att-equal)
 
 (is (parse-template-string "{{loop seq as i}}<li>item{{var i}}</li>{{endloop}}")
-    (att-progn (att-loop (att-variable 'seq)
+    (att-progn
+     (att-output (att-string ""))
+     (att-loop (att-variable 'seq)
                          (att-progn (att-output (att-string "<li>item"))
                                     (att-output (att-variable 'i))
                                     (att-output (att-string "</li>")))
-                         (att-variable 'i)))
+                         (att-variable 'i))
+     (att-output (att-string "")))
     "loop"
     :test #'att-equal)
 
 (is (parse-template-string "the content of foo is {{insert \"foo\"}}")
-    (att-progn (att-output (att-string "the content of foo is "))
-               (att-output (att-string "{{repeat 2 as i}}bar{{endrepeat}}")))
+    (att-progn
+     (att-output (att-string "the content of foo is "))
+     (att-output (att-string "{{repeat 2 as i}}bar{{endrepeat}}"))
+     (att-output (att-string "")))
     "insert"
     :test #'att-equal)
 
 (is (parse-template-string "the content of foo is {{include \"foo\"}}")
-    (att-progn (att-output (att-string "the content of foo is "))
-               (att-progn (att-loop
-                           (att-constant '(1 2))
-                           (att-progn (att-output (att-string "bar")))
-                           (att-variable 'i))))
+    (att-progn
+     (att-output (att-string ""))
+     (att-output
+      (att-string "the content of foo is "))
+     (att-progn
+      (att-output (att-string ""))
+      (att-loop
+       (att-constant '(1 2))
+       (att-progn (att-output (att-string "bar")))
+       (att-variable 'i))
+      (att-output (att-string "")))
+     (att-output (att-string "")))
     "include"
     :test #'att-equal)
 (finalize)
