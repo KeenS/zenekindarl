64e23986e9c5731ee09bfb2346fff76c183299e5
Sunrim KIM (keen) <3han5chou7@gmail.com>
2015-09-07 01:18:04 +0900

    add html-template benchmark

--- benchmark/bench.lisp
+++ benchmark/bench.lisp
@@ -5,11 +5,16 @@ Copyright (c) 2014 κeen
 
 
 ;;; To run benchmark, simply cd to here and load this file.
+(require 'asdf)
+(require 'zenekindarl)
+(require 'html-template)
 (in-package :cl-user)
 (defpackage zenekindarl-bench
   (:use
    :cl
-   :zenekindarl)
+   :zenekindarl
+
+   :html-template)
   (:import-from :fast-io
                 :with-fast-output)
   (:import-from :alexandria
@@ -30,12 +35,14 @@ Copyright (c) 2014 κeen
      (time (loop :repeat 10000 :do ,form))))
 
 (defmacro bench/zenekindarl (tmpl args)
-  (let ((stream-renderer       (compile-template-file :stream tmpl))
-        (octet-stream-renderer (compile-template-file :octet-stream tmpl))
-        (string-renderer       (compile-template-file :string tmpl))
-        (octets-renderer       (compile-template-file :octets tmpl))
-        (fast-io-renderer      (compile-template-file :fast-io tmpl)))
+  (let* ((tmpl (pathname (concatenate 'string tmpl ".zenekindarl")))
+         (stream-renderer       (compile-template-file :stream tmpl))
+         (octet-stream-renderer (compile-template-file :octet-stream tmpl))
+         (string-renderer       (compile-template-file :string tmpl))
+         (octets-renderer       (compile-template-file :octets tmpl))
+         (fast-io-renderer      (compile-template-file :fast-io tmpl)))
     `(progn
+       (write-line "zenekindarl")
        (with-open-file (/dev/null "/dev/null" :direction :output :if-exists :append)
          (bench10000 (format nil "compiled stream backend with ~a" ,tmpl)
                      (funcall ,stream-renderer /dev/null ,@args))
@@ -49,8 +56,23 @@ Copyright (c) 2014 κeen
          (bench10000 (format nil "compiled fast-io backend with ~a" ,tmpl)
                      (with-fast-output (buff /dev/null) (funcall ,fast-io-renderer buff ,@args)))))))
 
+(defmacro bench/html-template (tmpl args)
+  (let* ((args           (if (string= tmpl "repeat.tmpl")
+                             (list :foos (mapcar (lambda (x) (list :foo x)) (cadadr args)))
+                             args))
+         (tmpl (pathname (concatenate 'string tmpl ".html-template")))
+         (renderer       (create-template-printer tmpl)))
+    `(progn
+       (write-line "html-template")
+       (with-open-file (/dev/null "/dev/null" :direction :output :if-exists :append)
+         (let ((*default-template-output* /dev/null))
+          (bench10000 (format nil "render ~a" ,tmpl)
+                      (fill-and-print-template ,renderer ',args)))))))
+
 (defmacro bench (tmpl args)
-  `(bench/zenekindarl ,tmpl ,args))
+  `(progn
+     (bench/zenekindarl ,tmpl ,args)
+     (bench/html-template ,tmpl ,args)))
 
 #.`(progn
      ,@(loop :for (tmpl . args) :in *templates*
--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -6,7 +6,7 @@ CURRENT_BRANCH="$(git branch --list --no-color | grep '^\*' | cut -d\  -f2)"
 cd "${BENCH_ROOT}"
 
 bench_all(){
-    sbcl --eval "(require 'asdf)" --eval "(require 'zenekindarl)" --load bench.lisp --quit > "${CURRENT_BRANCH}_all.bench" 2>&1
+    sbcl  --load bench.lisp --quit > "${CURRENT_BRANCH}_all.bench" 2>&1
 }
 
 bench_all
